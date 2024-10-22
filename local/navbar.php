a692513ae5637a56388e0cf1df8564f0c547cba8
FelixKnutsen <Felixfk@uia.no>
2024-10-22 21:57:22 +0200

    fixed mvc structure and fixed pathing
    Co-authored-by: Christian <Lordkissa97@users.noreply.github.com>
    Co-authored-by: Adrian Kristoffer Viken <Adriankv1@users.noreply.github.com>

--- local/navbar.php
+++
@@ -1,9 +0,0 @@
-<nav class="navbar">
-    <ul>
-        <li><a href=".\..\..\php-prosjekt\index.php">Home</a></li>
-        <li><a href="./../../php-prosjekt/pages/rombooking.php"><strong>Book et rom her</strong></a></li>
-        <li><a href="./../../php-prosjekt/pages/login.php">Logg in</a><li>
-        <li><a href="./../../php-prosjekt/pages/registrer.php"> Registrering</a><li>
-    </ul>
-</nav>
-
---
+++ views/partials/navbar.php
@@ -0,0 +1,9 @@
+<nav class="navbar">
+    <ul>
+        <li><a href=".\..\../..\php-prosjekt\index.php">Home</a></li>
+        <li><a href="./../../../php-prosjekt/views/pages/rombooking.php"><strong>Book et rom her</strong></a></li>
+        <li><a href="./../../../php-prosjekt/views/pages/login.php">Logg in</a><li>
+        <li><a href="./../../../php-prosjekt/views/pages/registrer.php"> Registrering</a><li>
+    </ul>
+</nav>
+
