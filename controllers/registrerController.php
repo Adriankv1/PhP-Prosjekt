e0a053e6f1485023a00985c9328a67ee32f50058
Lordkissa97 <christiaea@uia.no>
2024-11-05 20:34:56 +0100

    fungerende database. Trenger å ordne slik at man ikke ender på error page etter trykking av registrer.

--- controllers/registrerController.php
+++ controllers/registrerController.php
@@ -1,39 +1,52 @@
-<?php include('server.php') ?> <!-- import server -->
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Registration system PHP and MySQL</title>
-  <link rel="stylesheet" type="text/css" href="style.css"> 
-</head>
-<body>
-  <div class="header"> 
-        <h2>Register</h2>
-  </div>
-        
-  <form method="post" action="register.php"> <!-- Form -->
-        <?php include('errors.php'); ?> <!-- Include errors -->
-        <div class="input-group"> <!-- Username group -->
-          <label>Username</label>
-          <input type="text" name="username" value="<?php echo $username; ?>"> <!-- Username input -->
-        </div>
-        <div class="input-group"> <!-- Email group -->
-          <label>Email</label>
-          <input type="email" name="email" value="<?php echo $email; ?>"> <!-- Email input -->
-        </div>
-        <div class="input-group"> <!-- Password group -->
-          <label>Password</label>
-          <input type="password" name="password_1"> <!-- Password input -->
-        </div>
-        <div class="input-group"> <!-- Confirm password group -->
-          <label>Confirm password</label>
-          <input type="password" name="password_2"> <!-- Confirm password input -->
-        </div>
-        <div class="input-group"> <!-- Button group -->
-          <button type="submit" class="btn" name="reg_user">Register</button> <!-- Register button -->
-        </div>
-        <p>
-                Already a member? <a href="login.php">Sign in</a> <!-- Sign in link -->
-        </p>
-  </form> 
-</body>
-</html>
+<?php
+include('../../config/server.php');
+
+// initializing variables
+$username = "";
+$email    = "";
+$errors = array(); 
+
+// REGISTER USER
+if (isset($_POST['reg_user'])) {
+  // receive all input values from the form
+  $username = mysqli_real_escape_string($db, $_POST['brukernavn']);
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+
+  // form validation: ensure that the form is correctly filled
+  // by adding (array_push()) corresponding error  $errors array
+  if (empty($username)) { array_push($errors, "Username is required"); }
+  if (empty($email)) { array_push($errors, "Email is required"); }
+  if (empty($password_1)) { array_push($errors, "Password is required"); }
+  if ($password_1 != $password_2) {
+        array_push($errors, "The two passwords do not match");
+  }
+
+  // Checks if user exists
+  $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+  
+  if ($user) { // if user exists
+    if ($user['username'] === $username) {
+      array_push($errors, "Username already exists");
+    }
+
+    if ($user['email'] === $email) {
+      array_push($errors, "Email already exists");
+    }
+  }
+
+  // Register user if there are no errors in the form
+  if (count($errors) == 0) {
+    $password = md5($password_1); //encrypt the password before saving in the database
+    $query = "INSERT INTO users (username, email, password) 
+              VALUES('$username', '$email', '$password')";
+    mysqli_query($db, $query);
+    $_SESSION['username'] = $username;
+    $_SESSION['success'] = "You are now logged in";
+    header('location: index.php');
+  }
+}
+?>
