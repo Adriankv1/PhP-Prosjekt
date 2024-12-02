08ce6100f9518bda4ec27230d4e2b7b3fa271a58
adriankv1 <adriankv@uia.no>
2024-12-02 22:42:35 +0100

    preference profile update

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -55,6 +55,7 @@ if (isset($_POST['login_user'])) {
                 $_SESSION['username'] = $user['username'];
                 $_SESSION['user_id'] = $user['id'];
                 $_SESSION['role'] = $user['role'];
+                $_SESSION['user'] = $user;
                 $_SESSION['success'] = "You are now logged in";
 
                 // Clear login attempts on successful login
